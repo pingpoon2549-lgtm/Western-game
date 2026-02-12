14a7157b0d60c13cd405dad5969d346900da01e3
beyourssun <pingpoon2549@gmail.com>
2026-02-12 16:43:25 +0700

    Camera & Background

--- code/main.py
+++ code/main.py
@@ -10,11 +10,11 @@ class Game :
         self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         pygame.display.set_caption('Western Shoter')
         self.clock = pygame.time.Clock()
-        self.all_sprites = pygame.sprite.Group()
+        self.all_sprites = AllSprites()
         self.setup()
 
     def setup(self):
-        Player((200, 200), self.all_sprites, PATHS['player'],None)
+        self.player = Player((200, 200), self.all_sprites, PATHS['player'],None)
 
     def run(self):
         while True:
@@ -31,9 +31,30 @@ class Game :
             self.all_sprites.update(dt)
 
             self.display_surface.fill('black')
-            self.all_sprites.draw(self.display_surface)
+            self.all_sprites.customize_draw(self.player)
+            #self.all_sprites.draw(self.display_surface)
 
             pygame.display.update()
+
+class AllSprites(pygame.sprite.Group) :
+    def __init__(self):
+        super().__init__()
+        self.display_surface = pygame.display.get_surface()
+        self.offset = vector()
+
+        self.bg = pygame.image.load('../graphics/other/bg.png').convert()
+
+    def customize_draw(self, player):
+        self.offset.x = player.rect.centerx - WINDOW_WIDTH/2
+        self.offset.y = player.rect.centery - WINDOW_HEIGHT/2
+        # -- วาดพื้นหลัง --
+        self.display_surface.blit(self.bg, -self.offset)
+        # -- วาด Sprite ทุกตัว --
+        for sprite in self.sprites():
+            offset_rect = sprite.image.get_rect(center = sprite.rect.center)
+            offset_rect.center -= self.offset
+            self.display_surface.blit(sprite.image, offset_rect)
+
 
 if __name__ == '__main__':
     game = Game()
