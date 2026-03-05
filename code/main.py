61bdadb51ed1aeb40e0cce2fdbb49a9f6198ef45
beyourssun <pingpoon2549@gmail.com>
2026-03-05 16:08:27 +0700

    bullet shoot

--- code/main.py
+++ code/main.py
@@ -1,4 +1,6 @@
 import pygame, sys
+from plotly.figure_factory import create_bullet
+
 from settings import *
 from player import *
 from pytmx.util_pygame import load_pygame
@@ -13,18 +15,24 @@ class Game :
         pygame.display.set_caption('Western Shoter')
         self.clock = pygame.time.Clock()
         self.all_sprites = AllSprites()
+        self.obstacles = pygame.sprite.Group()
+        self.bullet_surf = pygame.image.load('../graphics/other/particle.png')
+        self.bullets = pygame.sprite.Group()
         self.setup()
 
+
+    def create_bullet(self, pos, direction):
+        Bullet(pos, direction, self.bullet_surf, [self.all_sprites, self.bullets])
+
     def setup(self):
-        self.player = Player((200, 200), self.all_sprites, PATHS['player'],None)
         tmx_map = load_pygame('../data/map1.tmx')
         for x,y, surf in tmx_map.get_layer_by_name('Fence').tiles():
-            Sprite((x * 64, y * 64), surf, self.all_sprites)
+            Sprite((x * 64, y * 64), surf, [self.all_sprites, self.obstacles])
         for obj in tmx_map.get_layer_by_name('Object'):
-            Sprite((obj.x, obj.y), obj.image, self.all_sprites)
+            Sprite((obj.x, obj.y), obj.image, [self.all_sprites, self.obstacles])
         for obj in tmx_map.get_layer_by_name('Entities'):
             if obj.name == 'Player':
-                self.player = Player((obj.x, obj.y), self.all_sprites, PATHS['player'], None)
+                self.player = Player((obj.x, obj.y), self.all_sprites, PATHS['player'], self.obstacles, create_bullet = self.create_bullet)
 
     def run(self):
         while True:
@@ -65,6 +73,7 @@ class AllSprites(pygame.sprite.Group) :
             offset_rect = sprite.image.get_rect(center = sprite.rect.center)
             offset_rect.center -= self.offset
             self.display_surface.blit(sprite.image, offset_rect)
+
 
 if __name__ == '__main__':
     game = Game()
--- code/player.py
+++ code/player.py
@@ -1,9 +1,11 @@
+from unittest import case
+
 import pygame
 from pygame.math import Vector2 as vector
 from os import walk
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, pos, groups, path, collision_sprites):
+    def __init__(self, pos, groups, path, collision_sprites, create_bullet):
         super().__init__(groups)
         # -- image&rect--
         self.image = pygame.Surface((100, 100))
@@ -14,12 +16,14 @@ class Player(pygame.sprite.Sprite):
         self.direction = vector()
         self.speed = 200
         # -- collision(การชน) ---
-        self.hitbox = self.rect.inflate(0, -self.rect.height/2)
+        #self.hitbox = self.rect.inflate(0, -self.rect.height/2)
+        self.hitbox = self.rect.inflate(-self.rect.width * 0.5, -self.rect.height / 2)
         self.collision_sprites = collision_sprites
         self.imports_assets(path)
         self.status = 'down'
         self.frame_index = 0
         self.attacking = False
+        self.create_bullet = create_bullet
 
     def imports_assets(self, path):
         self.animations = {}
@@ -48,6 +52,13 @@ class Player(pygame.sprite.Sprite):
                 self.attacking = False
         self.image = current_animation[int(self.frame_index)]
 
+        # -- ยิงกระสุนเฟรมที่ 2 ---
+        if int(self.frame_index) == 2 and self.attacking and not self.bullet_shot:
+            bullet_start_pos = self.rect.center + self.bullet_direction * 80
+
+            self.create_bullet(bullet_start_pos, self.bullet_direction)
+            self.bullet_shot = True
+
     def input(self):
         keys = pygame.key.get_pressed()
 
@@ -74,6 +85,14 @@ class Player(pygame.sprite.Sprite):
                 self.attacking = True
                 self.direction = vector()
                 self.frame_index = 0
+                self.bullet_shot = False
+
+                # --กำหนดทิศทางกระสุน--
+                match self.status.split('_')[0]:
+                    case 'left' : self.bullet_direction = vector(-1,0)
+                    case 'right' : self.bullet_direction = vector(1,0)
+                    case 'up' : self.bullet_direction = vector(0,-1)
+                    case 'down' : self.bullet_direction = vector(0,1)
 
 
     def get_status(self):
@@ -89,10 +108,33 @@ class Player(pygame.sprite.Sprite):
         self.pos.x += self.direction.x * self.speed * dt
         self.hitbox.centerx = round(self.pos.x)
         self.rect.centerx = self.hitbox.centerx
+        self.collision('horizontal')
 
         self.pos.y += self.direction.y * self.speed * dt
         self.hitbox.centery = round(self.pos.y)
         self.rect.centery = self.hitbox.centery
+        self.collision('vertical')
+
+    def collision(self, direction):
+        # ---เช็คชน Hitox ---
+        for sprite in self.collision_sprites.sprites():
+            if hasattr(sprite, 'hitbox') and sprite.hitbox.colliderect(self.hitbox):
+                # --horizontal---
+                if direction == 'horizontal':
+                    if self.direction.x > 0:
+                        self.hitbox.right = sprite.hitbox.left
+                    if self.direction.x < 0:
+                        self.hitbox.left = sprite.hitbox.right
+                    self.rect.centerx = self.hitbox.centerx
+                    self.pos.x = self.hitbox.centerx
+                # ---Vertical---
+                else:
+                    if self.direction.y > 0:
+                        self.hitbox.bottom = sprite.hitbox.top
+                    if self.direction.y < 0:
+                        self.hitbox.top = sprite.hitbox.bottom
+                    self.rect.centery = self.hitbox.centery
+                    self.pos.y = self.hitbox.centery
 
     def update(self, dt):
         self.get_status()
